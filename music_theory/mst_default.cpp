944857017e682e918fe0fe81bf755da9bc6c9c00
vastopol <classicunix@gmail.com>
2016-10-07 05:36:15 +0000

    small stuffs

--- music_theory/mst_default.cpp
+++ music_theory/mst_default.cpp
@@ -12,26 +12,26 @@ using namespace std;
 
 int main()
 {
-
+    // //VARIABLES
     //vector<int> v = {0,1,2,3,4,5,6,7,8,9,10,11}; // chromatic scale from c
-
     vector<int> v = {0,4,7}; // Cmaj triad
 
-    //vector<int> v = {2,6,9}; // Dmaj triad
-
-    int n;
+    int n; // choice
 
     cout << "Before" << endl;
     display(v);
 
-        // // transposition
-        //transpose(v);
-
-        // // retrograde
-        //R(v);
-
-        // // inversion
-        //I(v);
+    cout << "Choose operation:\n" << "1. Transpose\n"
+         << "2. Inversion\n" << "3. Retrograde" << endl;
+    
+    cin >> n;
+    switch(n)
+    {
+        case 1: transpose(v); break;   // transposition
+        case 2: invert(v); break;      // inversion
+        case 3: R(v); break;           // retrograde
+        default: cout << "ERROR::unknown entry" << endl; break;
+    }
 
     cout << "After" << endl;
     display(v);
---
+++ repl/interpreter/arithmetic/test.cc
@@ -0,0 +1,19 @@
+// attempt 1 at postfix stack operation calc
+
+
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<cstdlib>
+#include<cstring>
+#include <stack>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+    
+    
+    return 0;
+}
